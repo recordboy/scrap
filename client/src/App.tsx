a86f4cfa74d432fde206276cfa0b5f905d84fd36
Record Man <ksj_design@naver.com>
2021-01-06 17:40:30 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import SearchForm from "./components/SearchForm";
+import SearchList from "./components/SearchList";
+import InputCheckbox from "./components/InputCheckbox";
+import "./App.css";
 
 function App() {
+  const [searchData, setSearchData] = useState([]);
+
+  const getSearchData = (data: string) => {
+    console.log("검색 문구: ", data);
+
+    fetch(`/api/data?searchText=${data}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setSearchData(data);
+
+        console.log("검색 결과: ", data);
+      });
+  };
+
   return (
-    <div className="App">
-      <button type="button" onClick={() => {
-        fetch('/api/data')
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-        });
-      }}>get data</button>
+    <div className="App" style={{ height: window.innerHeight }}>
+      <SearchForm getSearchData={getSearchData} />
+      <SearchList searchData={searchData} />
+      <InputCheckbox />
+      <InputCheckbox />
     </div>
   );
 }
