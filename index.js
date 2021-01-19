7be39bee2d129f3ae770c3626ffad7daf6f4eef6
recordboy <ksj_design@naver.com>
2021-01-19 16:26:22 +0900

    headless모드 변경

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const crawlingTag = {
 const getSearchData = async (portal, searchText) => {
   // 브라우저 실행, 옵션 headless모드
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
 
   // 브라우저 열기
