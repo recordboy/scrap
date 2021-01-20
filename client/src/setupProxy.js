a3e8432f8661796574d96d72702d1afab353f7a1
recordboy <ksj_design@naver.com>
2021-01-20 14:23:30 +0900

    수정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api/data",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://recordboy-scrap.herokuapp.com",
       changeOrigin: true,
     })
   );
